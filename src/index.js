3f589f43dd7c86609fd56748cd5af8a3d0c02ba8
Yuri Kochergin <kocherg1n@icloud.com>
2020-03-03 23:50:56 +0300

    adaptive fixed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 // JS
-import './js/'
+import './js/hamburger'
+import './js/accordion-team'
 
 // SCSS
 import './assets/scss/main.scss'
 
-// CSS (example)
-// import './assets/css/main.css'
 
-// Vue.js
-window.Vue = require('vue')
 
-// Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)
 
-// Vue init
-const app = new Vue({
-  el: '#app'
-})
---
+++ src/js/hamburger.js
@@ -0,0 +1,15 @@
+// menu-hamburger ===========================================
+
+const burgerButton = document.querySelector(".hamburger");
+const navigation = document.querySelector(".hamburger-nav__wrapper");
+const close = document.querySelector(".hamburger__close");
+
+burgerButton.addEventListener("click", function () {
+    navigation.classList.toggle("active");
+    document.body.classList.toggle("overflow");
+});
+
+close.addEventListener("click", function () {
+    navigation.classList.toggle("active");
+    document.body.classList.toggle("overflow");
+});
