c007686e46d6f0a2eca60e13c0f8af4caff8e74f
Yuri Kochergin <kocherg1n@icloud.com>
2020-03-05 15:25:42 +0300

    readme fix

--- src/index.js
+++ src/index.js
@@ -4,17 +4,4 @@ import './js/index'
 // SCSS
 import './assets/scss/main.scss'
 
-window.Vue = require('vue');
-
-// Vue components (for use in html)
-Vue.component('slider-burger', require('./components/slider-burger.vue').default);
-
-// Vue carousel
-Vue.use(VueCarousel);
-import VueCarousel from 'vue-carousel';
-
-// Vue init
-const app = new Vue({
-    el: '#app'
-});
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,6 @@ import './hamburger'
 import './accordion-team'
 import './accordion-menu'
 import './overlay-reviews'
+import './flickity-carousel'
+import './scrollTo'
+
